99f0bafc0de850e8b7e06e6dd954ac1d3cf90777
ghemsley <ghemsley@protonmail.ch>
2021-05-19 08:12:35 -0500

    commit tree c471e99bda566cabae38cb7e7facd56d1c1b2662

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+const map = (array, func) => {
+  const newArray = []
+  for (const element of array) {
+    newArray.push(func(element))
+  }
+  return newArray
+}
+
+const reduce = (array, func, start = 0) => {
+  let total = start ? start : 0
+  for (const element of array) {
+    if (typeof element === 'number' || typeof element === 'bigint') {
+      total += element
+    } else {
+      total = func(element, total)
+    }
+  }
+  return total
+}
